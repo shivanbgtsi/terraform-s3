3540a9d374d82a23e335f88be32a91122ded781a
Shivanagouda Govindagoudar <68856301+shivanbgtsi@users.noreply.github.com>
2024-05-22 11:40:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3-bucket" {
-  bucket = "test-s3-bucket-jenkins-test"
+  bucket = "test-s3-bucket-jenkins-test-devnxt"
   tags = {
     name = "Test"
     Environment = "Dev"
